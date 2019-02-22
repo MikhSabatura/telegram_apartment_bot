7910ef41e8d2d81578e7daab24a80b9e477c204d
MikhSabatura <mikhailsabatura@gmail.com>
2019-02-22 14:26:51 +0100

    just polling without webhooks

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const Stage = require("telegraf/stage");
 const auth = require("./auth");
 const AprtMenu = require("./aprtMenu");
 const UserMenu = require("./userMenu");
-// const express = require("express");
-// const expressApp = express();
+const express = require("express");
+const expressApp = express();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.telegram.setWebhook(`${process.env.BOT_URL}/bot${process.env.BOT_TOKEN}`);
-bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, process.env.PORT);
+// bot.telegram.setWebhook(`${process.env.BOT_URL}/bot${process.env.BOT_TOKEN}`);
+// bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, process.env.PORT);
 // expressApp.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
 
 const stage = new Stage([
@@ -30,10 +30,11 @@ bot.use(auth.authenticate);
 bot.use(auth.filterAuthorized);
 bot.use(stage.middleware());
 auth.addEventHandlers(bot);
+bot.startPolling();
 
-// expressApp.get("/", (req, res) => {
-//     res.send("Hello World!");
-// });
-// expressApp.listen(process.env.PORT, () => {
-//     console.log("Example app listening on port 3000!");
-// });
+expressApp.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+expressApp.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}!`);
+});
