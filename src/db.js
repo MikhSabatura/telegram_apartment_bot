34792bf8760c6e39640bfb595fa26fd16525816d
MikhSabatura <mikhailsabatura@gmail.com>
2019-02-25 00:57:17 +0100

    debug

--- src/db.js
+++ src/db.js
@@ -3,7 +3,11 @@ const connectionString = process.env.DATABASE_URL;
 console.log(connectionString);
 
 const pool = new Pool({
-    connectionString: connectionString
+    // connectionString: connectionString
+    user: "postgres",
+    host: "localhost",
+    database: "unity_bot_db",
+    port: 5432
 });
 console.log(pool);
 
