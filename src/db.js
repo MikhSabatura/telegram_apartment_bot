00536a9ef0a2b9f2aa4b37075c2916a5668110e7
MikhSabatura <mikhailsabatura@gmail.com>
2019-02-25 00:35:34 +0100

    debug

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL;
+console.log(connectionString);
 
 const pool = new Pool({
     connectionString: connectionString
