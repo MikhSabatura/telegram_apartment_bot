2ed4d55ab9aaeb03e84ea18220a305560171c8f1
MikhSabatura <mikhailsabatura@gmail.com>
2019-03-13 20:15:07 +0100

    Removed webhook-related code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 const Telegraf = require("telegraf");
 const session = require("telegraf/session");
 const Stage = require("telegraf/stage");
-// const fs = require("fs");
 const auth = require("./auth");
 const AprtMenu = require("./aprtMenu");
 const UserMenu = require("./userMenu");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.telegram.deleteWebhook();
-
-// const tlsOptions = {
-//     key: fs.readFileSync("YOURPRIVATE.key"),
-//     cert: fs.readFileSync("YOURPUBLIC.pem")
-// };
-
-// bot.telegram.setWebhook(`https://${process.env.BOT_URL}:8443/bot${process.env.BOT_TOKEN}`, {
-//     source: fs.readFileSync("YOURPUBLIC.pem")
-// });
 
 const stage = new Stage([
     AprtMenu.aprtSearchScene,
@@ -37,5 +27,4 @@ bot.use(auth.filterAuthorized);
 bot.use(stage.middleware());
 auth.addEventHandlers(bot);
 
-// bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, tlsOptions, 8443);
 bot.startPolling();
